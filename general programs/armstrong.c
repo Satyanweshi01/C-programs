662207a529bdf7d65e62217c49eb86ef8e758025
Prolay Mondal <prolaymondal047@gmail.com>
2026-06-12 19:19:28 +0530

    rename

--- general programs/armstrong.c
+++ general programs/armstrong.c
@@ -3,13 +3,17 @@
 int counter(int n, int num[]);
 void arm(int n, int counter, int num[]);
 int power(int base, int exponant);
+
 int main()
 {   
     int num[10], n,count=0;
-    printf("Enter the number: ");
-    scanf("%d",&n);
+    do
+    {
+        printf("Enter the number: ");
+        scanf("%d",&n);
+    }while(n<0);
     count = counter(n,num);
-    arm(n,count,num);
+    arm(n,count,num); // num holds address of first element of the array
     return 0;
 }
 int counter(int n, int num[])   // a function to get the number of digit and putting the digits in the num array
@@ -17,10 +21,12 @@ int counter(int n, int num[])   // a function to get the number of digit and put
     int i,mod,counter=0;
     for (i=0;n!=0;i++)
     {
-        mod = n%10;
-        num[i] = mod;
-        n = n - mod;
-        n = n/10;
+        num[i] = n % 10;
+        n /= 10;
+    //     mod = n%10;
+    //     num[i] = mod;
+    //     n = n - mod;
+    //     n = n/10;
         counter+=1;
     }
     return counter;
