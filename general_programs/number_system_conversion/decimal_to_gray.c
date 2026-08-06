e46b5e871e9da6f69f5bb2c00ea6e101fa27275b
Prolay Mondal <prolaymondal047@gmail.com>
2026-08-06 19:52:30 +0530

    number system conversion add

---
+++ general_programs/number_system_conversion/decimal_to_gray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define INTEGER 32
+int main()
+{
+    
+    int n,i,storage[INTEGER],result[INTEGER];
+    printf("Enter the decimal number: ");
+    scanf("%d",&n);
+    // 0 -> 0000
+    // 1 -> 0001
+    // 2 -> 0011
+    // 3 -> 0010
+    // 4 -> 0110
+    // 5 -> 0111
+    // addition of binary representaiton of the given decimal value
+    for (i = 0; i < sizeof(n) * 8 - 1; i++) { // sizeof(n) * 8 - 1 = (4*8) - 1
+        //printf("%d ", i);
+        int bit = (n >> i) & 1;
+        storage[i] = bit;
+    }
+    // gray code conversion
+    result[0] = storage[0];
+    for (i = 1; )
+    {
+
+    }
+    // printing the array
+    for (i = 0; i<INTEGER; i++)
+    {
+        printf("%d",storage[i]);
+    }
+    return 0;
+}
